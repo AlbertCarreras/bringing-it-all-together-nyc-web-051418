304a755ce02d078a6c8961e0612a182532f1f453
Alberto Carreras <a.carreras.c@gmail.com>
2018-05-23 05:45:57 -0400

    Add some adjustments

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,30 +30,34 @@ class Dog
   end
 
   def self.new_from_db(row)
-    hash = { name: row[1], breed: row[2] }
-    new_dog = Dog.new(hash)
+    new_hash = { name: row[1], breed: row[2] }
+    new_dog = Dog.new(new_hash)
     new_dog.id = row[0]
     new_dog
   end
 
   def save
-    if id
-      update
+    if self.id
+      self.update
     else
       sql = <<-SQL
       INSERT INTO dogs (name, breed)
       VALUES (?, ?);
       SQL
 
-      sql_id = <<-SQL
+      sql_last_id = <<-SQL
       SELECT last_insert_rowid() FROM dogs;
       SQL
 
       DB[:conn].execute(sql, @name, @breed)
 
-      @id =  DB[:conn].execute(sql_id)[0][0]
+      @id = DB[:conn].execute(sql_last_id)[0][0]
     end
     self
+  end
+
+  def self.create(hash)
+    new(hash).save
   end
 
   def self.find_by_name(name)
@@ -63,12 +67,7 @@ class Dog
       LIMIT 1;
     SQL
     searched_dog = DB[:conn].execute(sql, name)[0]
-    new_from_db(searched_dog)
-  end
-
-  def self.create(hash)
-    new_dog = new(hash)
-    new_dog.save
+    self.new_from_db(searched_dog)
   end
 
   def self.find_by_id(id)
@@ -78,7 +77,7 @@ class Dog
       LIMIT 1;
     SQL
     searched_dog = DB[:conn].execute(sql, id)[0]
-    new_from_db(searched_dog)
+    self.new_from_db(searched_dog)
   end
 
   def self.find_or_create_by(hash)
@@ -87,16 +86,12 @@ class Dog
     WHERE name = ? AND breed = ?;
     SQL
     dog = DB[:conn].execute(sql, hash[:name], hash[:breed])
-    # binding.pry
-    if !dog.empty?
 
-      dog_data = dog[0]
-      new_dog = new(name: dog_data[1], breed: dog_data[2])
-      new_dog.id = dog_data[0]
-    else
-      new_dog = create(hash)
+    if dog.empty? # does not exist yet
+      return self.create(hash)
+    else # it already exists
+      return self.new_from_db(dog[0])
     end
-    new_dog
   end
 
   def update
@@ -105,6 +100,7 @@ class Dog
       SET name = ?, breed = ?
       WHERE id = ?;
     SQL
-    DB[:conn].execute(sql, name, breed, id)
+
+    DB[:conn].execute(sql, @name, @breed, @id)
   end
 end
